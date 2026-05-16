6413758f6d50f6839102a4181ab866e7e56b4484
Tanmay <tannmay.chopade@lumiq.ai>
2026-05-16 18:07:40 +0530

    Patterns practiced

---
+++ Questions Python/0000-hello-world.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the Number: "))
+
+for i in range(1, n+1):
+    space = n-i
+    for k in range(1, space+1):
+        print(' ', end=' ')
+    for j in range(1, i+1):
+        print('*', end=' ')
+    print('\n')
