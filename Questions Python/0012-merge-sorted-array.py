06869d1b633649d34c7ba85cac27ab95481ab0f7
Tanmay <tanmaychopde2708@gmail.com>
2026-05-26 15:59:39 +0530

    Question Solved: Merge Sorted Array in Python

---
+++ Questions Python/0012-merge-sorted-array.py
@@ -0,0 +1,58 @@
+# Problem URL : https://leetcode.com/problems/merge-sorted-array/description/
+# Author      : Tanmay Chopade
+# Solved On   : 2026-05-26
+# Last Updated: [Optional: Last modification date or version number]
+'''
+/**************************************************************************************************
+ *
+ * Problem Description:
+ * -----------------------------------------------------------------------------------------------
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, 
+ * representing the number of elements in nums1 and nums2 respectively.
+ * 
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ * 
+ * The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
+ * To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, 
+ * and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ *
+ * Example:
+ * -----------------------------------------------------------------------------------------------
+ * Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * Output: [1,2,2,3,5,6]
+ *
+ * Constraints::
+ * -----------------------------------------------------------------------------------------------
+ * nums1.length == m + n
+ * nums2.length == n
+ * 0 <= m, n <= 200
+ * 1 <= m + n <= 200
+ * -109 <= nums1[i], nums2[j] <= 109
+ *
+ **************************************************************************************************/
+'''
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i, j = m-1,n-1
+        k = m + n - 1
+        while i >=0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                k -= 1
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                k -= 1
+                j -= 1
+        while i >=0:
+            nums1[k] = nums1[i]
+            k -= 1
+            i -= 1
+        while j >=0:
+            nums1[k] = nums2[j]
+            k -= 1
+            j -= 1
