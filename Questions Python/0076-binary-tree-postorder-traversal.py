fdcf991ae8553edba468963d06755b7b3db0233a
Tanmay <tanmaychopde2708@gmail.com>
2026-06-11 14:52:29 +0530

    Question Solved: Binary Tree Postorder Traversal in C++ and Python

---
+++ Questions Python/0076-binary-tree-postorder-traversal.py
@@ -0,0 +1,70 @@
+# Problem URL : https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+# Author      : Tanmay Chopade
+# Solved On   : 2026-06-11
+# Last Updated: [Optional: Last modification date or version number]
+'''
+/**************************************************************************************************
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+Input: root = [1,null,2,3]
+
+Output: [3,2,1]
+
+Explanation:
+
+
+
+Example 2:
+
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+
+Output: [4,6,7,5,2,9,8,3,1]
+
+Explanation:
+
+
+
+Example 3:
+
+Input: root = []
+
+Output: []
+
+Example 4:
+
+Input: root = [1]
+
+Output: [1]
+
+ 
+
+Constraints:
+
+The number of the nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+**************************************************************************************************/
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorder(self, root, ans):
+        if root is None:
+            return
+        
+        self.postorder(root.left, ans)
+        self.postorder(root.right, ans)
+        ans.append(root.val)
+
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        self.postorder(root, ans)
+        return ans
