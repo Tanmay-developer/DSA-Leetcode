b5b582b2b2f14401762c37242bafa365ccc85488
Tanmay <tanmaychopde2708@gmail.com>
2026-06-12 14:40:53 +0530

    Question Solved: Search in a Binary Search Tree in C++ and Python

---
+++ Questions Python/0086-search-in-a-binary-search-tree.py
@@ -0,0 +1,45 @@
+# Problem URL : https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+# Author      : Tanmay Chopade
+# Solved On   : 2026-06-12
+# Last Updated: [Optional: Last modification date or version number]
+'''
+/**************************************************************************************************
+You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. 
+If such a node does not exist, return null.
+
+ 
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
+Example 2:
+
+
+Input: root = [4,2,7,1,3], val = 5
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 5000].
+1 <= Node.val <= 107
+root is a binary search tree.
+1 <= val <= 107
+**************************************************************************************************/
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        while root is not None and root.val != val:
+            root = root.left if val < root.val else root.right
+        return root
