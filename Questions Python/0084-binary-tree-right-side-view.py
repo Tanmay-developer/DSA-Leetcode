baae8804e4d023026f5f15546c6470c6bc7d4b42
Tanmay <tanmaychopde2708@gmail.com>
2026-06-12 13:14:18 +0530

    Question Solved: Binary Tree Right Side View in C++ and Python

---
+++ Questions Python/0084-binary-tree-right-side-view.py
@@ -0,0 +1,71 @@
+# Problem URL : https://leetcode.com/problems/binary-tree-right-side-view/description/
+# Author      : Tanmay Chopade
+# Solved On   : 2026-06-12
+# Last Updated: [Optional: Last modification date or version number]
+'''
+/**************************************************************************************************
+Given the root of a binary tree, imagine yourself standing on the right side of it, return the values 
+of the nodes you can see ordered from top to bottom.
+
+ 
+
+Example 1:
+
+Input: root = [1,2,3,null,5,null,4]
+
+Output: [1,3,4]
+
+Explanation:
+
+
+
+Example 2:
+
+Input: root = [1,2,3,4,null,null,null,5]
+
+Output: [1,3,4,5]
+
+Explanation:
+
+
+
+Example 3:
+
+Input: root = [1,null,3]
+
+Output: [1,3]
+
+Example 4:
+
+Input: root = []
+
+Output: []
+
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+**************************************************************************************************/
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def solve(self, root, ans, level):
+        if not root:
+            return
+        if len(ans) == level:
+            ans.append(root.val)
+        self.solve(root.right, ans, level+1)
+        self.solve(root.left, ans, level+1)
+
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        self.solve(root, ans, 0)
+        return ans
